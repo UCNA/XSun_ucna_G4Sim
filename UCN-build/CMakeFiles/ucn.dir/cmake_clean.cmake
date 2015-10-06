0ff7816accfc7b2bf67ecbc2b3976d1ba36437d9
Xuan Sun <xuan.sun1@gmail.com>
2015-10-06 12:55:13 -0700

    All project files renamed to UCN and executable renamed as ucn. CMakelists modified to account for this.
    Everything OK still - compiles and executes without issue.

---
+++ UCN-build/CMakeFiles/ucn.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ucn.dir/ucn.cc.o"
+  "CMakeFiles/ucn.dir/src/B1EventAction.cc.o"
+  "CMakeFiles/ucn.dir/src/B1Run.cc.o"
+  "CMakeFiles/ucn.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/ucn.dir/src/B1DetectorConstruction.cc.o"
+  "CMakeFiles/ucn.dir/src/B1ActionInitialization.cc.o"
+  "CMakeFiles/ucn.dir/src/B1RunAction.cc.o"
+  "CMakeFiles/ucn.dir/src/B1SteppingAction.cc.o"
+  "ucn.pdb"
+  "ucn"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ucn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
