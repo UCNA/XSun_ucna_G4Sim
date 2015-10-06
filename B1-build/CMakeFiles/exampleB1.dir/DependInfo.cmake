72cac12222d89abc22b071835effd1c0ff2aa0a0
Xuan Sun <xuan.sun1@gmail.com>
2015-10-06 12:11:46 -0700

    Built example B1 in the local repo. Tested with visualization and successful rendering using OpenGL.
    cmaketext.txt was modified to include the new path with a local repo.

---
+++ B1-build/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1/exampleB1.cc" "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1-build/CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1/src/B1ActionInitialization.cc" "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1-build/CMakeFiles/exampleB1.dir/src/B1ActionInitialization.cc.o"
+  "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1/src/B1DetectorConstruction.cc" "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1-build/CMakeFiles/exampleB1.dir/src/B1DetectorConstruction.cc.o"
+  "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1/src/B1EventAction.cc" "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1-build/CMakeFiles/exampleB1.dir/src/B1EventAction.cc.o"
+  "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1/src/B1PrimaryGeneratorAction.cc" "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1-build/CMakeFiles/exampleB1.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1/src/B1Run.cc" "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1-build/CMakeFiles/exampleB1.dir/src/B1Run.cc.o"
+  "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1/src/B1RunAction.cc" "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1-build/CMakeFiles/exampleB1.dir/src/B1RunAction.cc.o"
+  "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1/src/B1SteppingAction.cc" "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1-build/CMakeFiles/exampleB1.dir/src/B1SteppingAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/xuansun/geant4/geant4-10.01.p02-install/include/Geant4"
+  "/home/xuansun/Documents/Caltech/UCNA_Sim/B1Example/B1/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
